59dde0cb07bb4727ec35338a9686bb0d5589f0f3
ferry <ferryxo@yahoo.com>
2016-09-08 20:35:14 -0400

    prototype of 2 ways communication. first the user needs to POST the JSON configuration and get an id back. he then can call the UI using that ID.

--- RainbowGraphService.py
+++ RainbowGraphService.py
@@ -1,12 +1,25 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS, cross_origin
+import uuid
+import json
 
 app = Flask(__name__)
+CORS(app)
 
+configTable = {}
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/configure', methods=['POST'])
+@cross_origin()
 def index():
-    return render_template('index.html', json_data = request.json)
+    id = uuid.uuid4();
+    configTable[id.urn[9:]] = request.json
+    return jsonify(id=id)
 
+@app.route('/vis/<id>', methods=['GET'])
+
+def visualize(id):
+ config = configTable[id]
+ return render_template('index.html', json_data = json.dumps(config))
 
 if __name__ == '__main__':
     app.run(debug=True)
