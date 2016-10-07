db175ff593c3a966d539aa34e240c32a3e0220c4
ferry <ferryxo@yahoo.com>
2016-10-07 15:11:45 -0400

    remove unused encoding code

--- RainbowGraphService.py
+++ RainbowGraphService.py
@@ -233,18 +233,6 @@ def visualize(id):
         return render_template('index.html', json_data = json.dumps(config))
 
 
-def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
-    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
-    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
-                            dialect=dialect, **kwargs)
-    for row in csv_reader:
-        # decode UTF-8 back to Unicode, cell by cell:
-        yield [unicode(cell, 'utf-8') for cell in row]
-
-def utf_8_encoder(unicode_csv_data):
-    for line in unicode_csv_data:
-        yield line.encode('utf-8')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3005, threaded=True)
